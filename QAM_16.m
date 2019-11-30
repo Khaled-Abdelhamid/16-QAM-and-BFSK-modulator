bda6c44d9f3b6e64c4dc559bef013402df1ff570
Khaled Abdelhamid <khaledusama1998@gmail.com>
2019-11-30 11:29:22 +0200

    Update QAM_16.m

--- QAM_16.m
+++ QAM_16.m
@@ -2,6 +2,7 @@ M = 16;                     % Size of signal constellation
 k = log2(M);                % Number of bits per symbol
 n = 200;                  % Number of bits to process
 numSamplesPerSymbol = 1;    % Oversampling factor
+
 stem(dataIn(1:40),'filled');
 title('Random Bits');
 xlabel('Bit Index');
